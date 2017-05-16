68c865b227c00d3149d6d7f1676b2825ca3fe834
jielong.lin <jielong.lin@tpv-tech.com>
2017-05-16 10:23:05 +0800

    =========================================================================  Submit the below CHANGES by jll.god.sh @2017-05-16 10:23:05 -------------------------------------------------------------------------  M jll.manual.backtrace.sh

--- jll.manual.backtrace.sh
+++ jll.manual.backtrace.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-05-16 09:56:24
-#   ModifiedTime: 2017-05-16 09:56:37
+#   ModifiedTime: 2017-05-16 10:20:16
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -17,6 +17,33 @@ source ${JLLPATH}/BashShellLibrary
 
 more >&1<<EOF
 
+
+${AC}${Fred}
+backtrace:
+    #00 pc 000423c4  /system/lib/libc.so (tgkill+12)
+    #01 pc 0003ffd1  /system/lib/libc.so (pthread_kill+32)
+    #02 pc 0001c75f  /system/lib/libc.so (raise+10)
+    #03 pc 00019911  /system/lib/libc.so (__libc_android_abort+34)
+    #04 pc 000174d0  /system/lib/libc.so (abort+4)
+    #05 pc 003b7158  /system/lib/libffmpeg_avcodec.so
+    #06 pc 001a9440  /system/lib/libffmpeg_avcodec.so
+    #07 pc 004a5e80  /system/lib/libffmpeg_avcodec.so (avcodec_decode_video2+552)
+    #08 pc 000aa6dc  /system/lib/libffmpeg_avformat.so
+    #09 pc 000b1724  /system/lib/libffmpeg_avformat.so (avformat_find_stream_info+2740)
+${Fyellow}
+Call Flow:
+    --->#09: avformat_find_stream_info
+        --->#08: unknown function (As logically Analyzed, it should be try_decode_frame)
+            --->#07: avcodec_decode_video2
+                --->#06: unknown function
+                    --->#05: unknown function
+                        ...
+${Fseablue}
+    I think the below cases perhaps display none in backtrace:
+    (1): static function, such as try_decode_frame
+    (2): function pointer
+    (3): not function prototype
+${AC}
 
 EOF
 
