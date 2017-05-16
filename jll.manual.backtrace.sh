2447093b123880d9bc1734875d5dccffad5180b9
jielong.lin <jielong.lin@tpv-tech.com>
2017-05-16 09:57:03 +0800

    Changes as follows: ?? jll.manual.backtrace.sh

---
+++ jll.manual.backtrace.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  jielong.lin.  All rights reserved.
+#
+#   FileName:     jll.manual.backtrace.sh
+#   Author:       jielong.lin
+#   Email:        493164984@qq.com
+#   DateTime:     2017-05-16 09:56:24
+#   ModifiedTime: 2017-05-16 09:56:37
+
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+source ${JLLPATH}/BashShellLibrary
+
+### Color Echo Usage ###
+# Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
+# echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
+
+more >&1<<EOF
+
+
+EOF
+
