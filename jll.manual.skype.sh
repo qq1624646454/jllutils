cf062f418330c7880f559916ef35809d6cd9babf
jielong.lin <493164984@qq.com>
2017-05-25 09:41:53 +0800

    =========================================================================  Submit the below CHANGES by jll.god.sh @2017-05-25 09:41:53 ------------------------------------------------------------------------- ?? jll.manual.skype.sh

---
+++ jll.manual.skype.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  jielong.lin.  All rights reserved.
+#
+#   FileName:     jll.manual.skype.sh
+#   Author:       jielong.lin
+#   Email:        493164984@qq.com
+#   DateTime:     2017-05-25 09:38:47
+#   ModifiedTime: 2017-05-25 09:41:41
+
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+source ${JLLPATH}/BashShellLibrary
+
+more >&1<<EOF
+
+${AC}${Fseablue}
+skype:
+Account={49...84@qq.com, X[Six.Birth.For.us][My.Name.Abbr]}
+
+
+${AC}
+
+EOF
+
