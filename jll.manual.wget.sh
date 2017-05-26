69f05e16832d549562df6ff087205be34c3ff5a1
jielong.lin <493164984@qq.com>
2017-05-26 09:44:03 +0800

    Changes as follows:  M jll.manual.wget.sh

--- jll.manual.wget.sh
+++ jll.manual.wget.sh
@@ -5,11 +5,11 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-03-09 15:56:24
-#   ModifiedTime: 2017-03-09 15:59:09
+#   ModifiedTime: 2017-05-26 09:43:06
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
-#source ${JLLPATH}/BashShellLibrary
+source ${JLLPATH}/BashShellLibrary
 
 ### Color Echo Usage ###
 # Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
@@ -17,15 +17,20 @@ JLLPATH="$(dirname ${JLLPATH})"
 
 cat >&1 <<EOF
 
-# Obtain the website specified by URL
+${AC}${Fseablue}
+# Obtain the website specified by URL${AC}
 wget -c -r -np -k --timeout=3  URL
+${AC}${Fseablue}
   e.g:
       wget -c -r -np -k --timeout=3   www.baidu.com
       w3m www.baidu.com/index.html
+${AC}
 
 
-==================================
-jielong.lin@TpvServer:~/test$ wget --no-check-certificate https://developers.tpvision.com/istreams/data/2017-05-25_14-05-53_TPVisionDebug.zip
+
+${AC}${Fwhite}
+==================================${AC}
+jielong.lin@TpvServer:~/test\$${Fyellow}wget --no-check-certificate https://developers.tpvision.com/istreams/data/2017-05-25_14-05-53_TPVisionDebug.zip${AC}
 --2017-05-26 09:33:09--  https://developers.tpvision.com/istreams/data/2017-05-25_14-05-53_TPVisionDebug.zip
 Resolving developers.tpvision.com (developers.tpvision.com)... 172.27.221.45
 Connecting to developers.tpvision.com (developers.tpvision.com)|172.27.221.45|:443... connected.
