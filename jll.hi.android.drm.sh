f06fd8ba38968f399897c141f4d9f62173a2a97a
jielong.lin <493164984@qq.com>
2017-06-01 21:28:04 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-01 21:27:01
+#   ModifiedTime: 2017-06-01 21:27:02
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -498,7 +498,7 @@ echo
 #
 # Find the legal SDK packages from the current.
 #
-declare -a __szConsiderFiles 
+declare -a CONF_lstFile 
 case ${__DRM_SCHEME} in
 playready)
     
