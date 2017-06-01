f3c95b90ab3d1008778192ed774803904821d09b
jielong.lin <493164984@qq.com>
2017-06-01 21:24:04 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-01 21:22:13
+#   ModifiedTime: 2017-06-01 21:23:50
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -28,9 +28,15 @@ if [ x"${__CvPathFileForScript}" != x ]; then
     else
         __CvScriptPath="$(cd ${__CvPathFileForScript};pwd)"
     fi
+    if [ x"${__CvScriptName}" = x ]; then
+        echo
+        echo "JLL-Exit: Not recognize the command \"$0\", then exit - 0"
+        echo
+        exit 0
+    fi 
 else
     echo
-    echo "JLL-Exit: Not recognize the command \"$0\", then exit"
+    echo "JLL-Exit: Not recognize the command \"$0\", then exit - 1"
     echo
     exit 0
 fi
