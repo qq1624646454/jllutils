71e005c661c551375017406be779c3542694a4e1
jielong.lin <493164984@qq.com>
2017-06-02 09:44:03 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-02 09:42:49
+#   ModifiedTime: 2017-06-02 09:43:49
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -529,7 +529,7 @@ widevine)
 esac
 
 for ((i=0;i<CONF_szFile;i++)) {
-    __listPath=$(find ${GvPrjRootPath} )
+    __=$(find ${GvPrjRootPath} )
 }
  
 
