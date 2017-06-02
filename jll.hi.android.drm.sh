55b4d0897c39957b898ef75975cefa6fa3afe353
jielong.lin <493164984@qq.com>
2017-06-02 15:48:03 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-02 15:46:44
+#   ModifiedTime: 2017-06-02 15:46:45
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -592,7 +592,7 @@ fi
     "JLL-Exit: Not found any legal Resources then exit"; exit 0
 
 clear
-[ x"${__lstResSZ}" -lt 1 ] && __Lfn_Sys_ColorEcho ${__CvBgSeaBule} ${__CvFgBlack} \
+__Lfn_Sys_ColorEcho ${__CvBgSeaBule} ${__CvFgBlack} \
     "======== The Legal Resources As Follows ========"
 for((i=0;i<__lstResSZ;i++)) {
     echo "  ${__lstRes[i]}"
