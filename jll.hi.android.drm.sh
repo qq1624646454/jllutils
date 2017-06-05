bb5b51167ede3833403cb353371e1119714c4fd2
jielong.lin <493164984@qq.com>
2017-06-05 18:34:04 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-05 18:30:33
+#   ModifiedTime: 2017-06-05 18:30:35
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -359,6 +359,7 @@ function Lfn_File_SearchSymbol_EX()
     fi
 
     if [ -z "${LvFssSymbol}" -o -z "${LvFssFile}" ]; then
+        echo "JLL-Return: LvFssSymbol=${LvFssSymbol} or LvFssFile=${LvFssFile}"
         __Lfn_Sys_FuncComment
         return;
     fi
