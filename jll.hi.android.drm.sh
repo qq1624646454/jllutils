cb8cf85259302e97c43811c960c67151ef9c244c
jielong.lin <493164984@qq.com>
2017-06-05 18:28:03 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-05 18:26:38
+#   ModifiedTime: 2017-06-05 18:27:39
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -449,7 +449,7 @@ function Lfn_File_SearchSymbol_EX()
                         fi
                     else
                         echo
-                        echo "JLL-Warning: Matched Line But not obtain the line number"
+                        echo "JLL-Warning| Matched Line But not obtain the line number:"
                         echo "${LvFssM}"
                         [ x"${__lstRanges}" != x ] && unset __lstRanges
                         [ x"${__iRanges}" != x ] && unset __iRanges
