6069b338e1e6e352b49099778830d10a83496ca5
jielong.lin <493164984@qq.com>
2017-06-05 21:36:03 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-05 21:34:20
+#   ModifiedTime: 2017-06-05 21:35:24
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -556,6 +556,9 @@ function Lfn_File_SearchSymbol_EX()
                     __lstSegment[__iSegment++]=${__SegSP}
                     __lstSegment[__iSegment++]=${__SegEP}
                 fi
+
+                # Remark the high-light keywords
+
 
                 if [ x"${JLLCFG_dbgEnable}" == x"2" ]; then
                     echo "<<<<< Obtaining the below ranges:"
