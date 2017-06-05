37971e3a9f21b5895ffec6b5ae1e97a2096c2706
jielong.lin <493164984@qq.com>
2017-06-05 21:31:03 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-05 21:18:55
+#   ModifiedTime: 2017-06-05 21:30:22
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -554,7 +554,6 @@ function Lfn_File_SearchSymbol_EX()
                     __lstSegment[__iSegment++]=${__SegEP}
                 fi
 
-echo "JLLing: $__iRanges; $__iSegment"
                 echo "+++++++++++++++++++++++++++++"
                 for((iTS=0;iTS<__iSegment;iTS+=2)) {
                     echo "New-ITEM: ${__lstSegment[iTS]}===${__lstSegment[iTS+1]}"
