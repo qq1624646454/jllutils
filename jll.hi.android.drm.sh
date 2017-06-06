cb21cf2161691129690206d2f446d51fc2ea2dc0
jielong.lin <493164984@qq.com>
2017-06-06 09:37:03 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-06 09:35:33
+#   ModifiedTime: 2017-06-06 09:36:17
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -593,7 +593,9 @@ function Lfn_File_SearchSymbol_EX()
                 for((iSR=0;iSR<__iSegment;iSR+=2)) {
                     __iRSP=${__lstSegment[iSR]}
                     __iREP=${__lstSegment[iSR+1]}
-                    while [ ${__lstSegment[iSR]}  ]
+                    while [ ${__iRSP} -le ${__iREP} ]; do 
+                       
+                    done
                 } 
             fi
         }
