5da411ef0e76dd0c5f9d2e6159c1cd43bb16ba12
jielong.lin <493164984@qq.com>
2017-06-06 09:21:03 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-06 09:00:47
+#   ModifiedTime: 2017-06-06 09:20:40
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -556,7 +556,8 @@ function Lfn_File_SearchSymbol_EX()
                     __lstSegment[__iSegment++]=${__SegSP}
                     __lstSegment[__iSegment++]=${__SegEP}
                 else ### else for if [ ${__iRanges} -gt 3 ]; then
-
+                    __lstSegment[__iSegment++]=${__SegSP}
+                    __lstSegment[__iSegment++]=${__SegEP}
                 fi ### end for if [ ${__iRanges} -gt 3 ]; then
 
                 if [ x"${JLLCFG_dbgEnable}" == x"2" ]; then
