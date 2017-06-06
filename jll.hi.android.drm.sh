19960e9cc6f949cb91c62f9b68a8fd1403290995
jielong.lin <493164984@qq.com>
2017-06-06 09:33:03 +0800

    Changes as follows:  M jll.hi.android.drm.sh

--- jll.hi.android.drm.sh
+++ jll.hi.android.drm.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-01 19:43:06
-#   ModifiedTime: 2017-06-06 09:27:44
+#   ModifiedTime: 2017-06-06 09:30:04
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
@@ -586,6 +586,11 @@ function Lfn_File_SearchSymbol_EX()
                         echo "Key: ${__lstRanges[iKK]}"
                     }
                 fi
+
+                #
+                # Rendering the result with keyword color
+                #
+                
             fi
         }
     }
