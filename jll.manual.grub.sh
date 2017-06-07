3ef17d3f113986c2487177d9a49712960205e1be
jielong.lin <493164984@qq.com>
2017-06-07 15:29:04 +0800

    Changes as follows:  M jll.manual.grub.sh

--- jll.manual.grub.sh
+++ jll.manual.grub.sh
@@ -8,11 +8,27 @@ source ${JLLPATH}/BashShellLibrary
 
 more >&1 << EOF
 
+======================================================================================
+||  How to set default item for grub
+======================================================================================
 jll@S~\$ ${Fyellow}su - root${AC}
- # cat /boot/grub/grub.cfg | grep Windows
- # grub-set-default "Windows 7 (loader) (on /dev/sda1)"
- # grub-editenv list
- # grub-mkconfig -o /boot/grub/grub.cfg
+jll@S~\# ${Fyellow}vim /etc/default/grub ${AC}
+...
+${Bpink}${Fblue}GRUB_DEFAULT=0${AC}
+...
+
+
+BUT, The below commands is invalid for ubuntu 12.10 After checking 
+----------------------------------------------------------------------------------
+jll@S~\$ ${Fyellow}su - root${AC}
+jll@S~\# ${Fyellow}cat /boot/grub/grub.cfg | grep Windows${AC}
+...
+jll@S~\# ${Fyellow} # grub-set-default "Windows 7 (loader) (on /dev/sda1)"${AC}
+...
+jll@S~\# ${Fyellow} grub-editenv list${AC}
+...
+jll@S~\# ${Fyellow} grub-mkconfig -o /boot/grub/grub.cfg${AC}
+
 
 EOF
 
