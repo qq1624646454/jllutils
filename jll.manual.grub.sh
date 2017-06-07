3536978d583fc4bfde1f736a8e9be937ff876957
jielong.lin <493164984@qq.com>
2017-06-07 15:24:04 +0800

    Changes as follows:  M jll.manual.grub.sh

--- jll.manual.grub.sh
+++ jll.manual.grub.sh
@@ -2,8 +2,13 @@
 # Copyright (c) 2016 - 2100.   jielong.lin     All rights reserved.
 #
 
-cat >&1 << EOF
- $ su - root
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+source ${JLLPATH}/BashShellLibrary
+
+more >&1 << EOF
+
+jll@S~\$ ${Fyellow}su - root${AC}
  # cat /boot/grub/grub.cfg | grep Windows
  # grub-set-default "Windows 7 (loader) (on /dev/sda1)"
  # grub-editenv list
