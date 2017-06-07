45e54584b8c323b772c1598e6e4c231f80ede7c3
jielong.lin <493164984@qq.com>
2017-06-07 15:31:03 +0800

    Changes as follows:  M jll.manual.grub.sh

--- jll.manual.grub.sh
+++ jll.manual.grub.sh
@@ -12,7 +12,7 @@ more >&1 << EOF
 ||  How to set default item for grub
 ======================================================================================
 jll@S~\$ ${Fyellow}su - root${AC}
-jll@S~\# ${Fyellow}vim /etc/default/grub ${AC}
+jll@S~# ${Fyellow}vim /etc/default/grub ${AC}
 ...
 ${Bpink}${Fseablue}GRUB_DEFAULT=0 ${AC}
 ...
@@ -21,13 +21,13 @@ ${Bpink}${Fseablue}GRUB_DEFAULT=0 ${AC}
 BUT, The below commands is invalid for ubuntu 12.10 After checking 
 ----------------------------------------------------------------------------------
 jll@S~\$ ${Fyellow}su - root${AC}
-jll@S~\# ${Fyellow}cat /boot/grub/grub.cfg | grep Windows${AC}
+jll@S~# ${Fyellow}cat /boot/grub/grub.cfg | grep Windows${AC}
 ...
-jll@S~\# ${Fyellow} # grub-set-default "Windows 7 (loader) (on /dev/sda1)"${AC}
+jll@S~# ${Fyellow}grub-set-default "Windows 7 (loader) (on /dev/sda1)"${AC}
 ...
-jll@S~\# ${Fyellow} grub-editenv list${AC}
+jll@S~# ${Fyellow}grub-editenv list${AC}
 ...
-jll@S~\# ${Fyellow} grub-mkconfig -o /boot/grub/grub.cfg${AC}
+jll@S~# ${Fyellow}grub-mkconfig -o /boot/grub/grub.cfg${AC}
 
 
 EOF
