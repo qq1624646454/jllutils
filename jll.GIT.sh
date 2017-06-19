498c81670e9400bfd7cb94815fa4f27841f34e17
jielong.lin <493164984@qq.com>
2017-06-19 18:28:04 +0800

    Changes as follows:  M jll.GIT.sh

--- jll.GIT.sh
+++ jll.GIT.sh
@@ -3,11 +3,8 @@
 #
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
-#source ${JLLPATH}/BashShellLibrary
+source ${JLLPATH}/BashShellLibrary
 
-### Color Echo Usage ###
-# Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
-# echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
 
 case x"$1" in
 x"push")
