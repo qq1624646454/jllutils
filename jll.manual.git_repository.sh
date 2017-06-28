8d7b42c353c00ca6000568ec0482a930279d3560
jielong.lin <493164984@qq.com>
2017-06-28 18:24:03 +0800

    Changes as follows:  M jll.manual.git_repository.sh

--- jll.manual.git_repository.sh
+++ jll.manual.git_repository.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-28 16:43:38
-#   ModifiedTime: 2017-06-28 18:10:04
+#   ModifiedTime: 2017-06-28 18:23:49
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -40,7 +40,16 @@ ${Fyellow} apt-get install apache2 ${AC}
 ${Fyellow} ln -s /usr/share/gitweb /var/www/ ${AC}
 
 
-
+${Fyellow} vim /etc/gitweb.conf ${AC}
+  1 # path to git projects (<project>.git)
+  2 \$projectroot = "${Fyellow}/home/jielong.lin/localhost${AC}";
+  3
+  4 # directory to use for temp files
+  ...
+ 31
+ 32 ${Fyellow}\$site_name = "GitWeb @ TPV-Server";${AC}
+ 33 ${Fyellow}\$feature{'search'}{'default'} = [1]; ${AC}
+ 34 ${Fyellow}\$feature{'blame'}{'default'} = [1]; ${AC}
 
 
 
