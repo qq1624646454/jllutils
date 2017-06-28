a5815e43df96c777c4fb58530056f4b61f06af0f
jielong.lin <493164984@qq.com>
2017-06-28 15:00:03 +0800

    Changes as follows:  M jll.manual.jllutils.sh ?? AUTO_TOOL_OVER_LINUX.tgz

--- jll.manual.jllutils.sh
+++ jll.manual.jllutils.sh
@@ -3,27 +3,36 @@
 #
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
-#source ${JLLPATH}/BashShellLibrary
+source ${JLLPATH}/BashShellLibrary
 
 more >&1 << EOF
 
+${Fpink}# Please install git tool ${AC}
 if [ x"\$(dpkg --get-selections | grep git)" = x ]; then \\
     aptitude install -y git git-svn git-doc git-email git-gui gitk gitweb \\
 fi
 
-mkdir -pv ~/github
+mkdir -pv ~/github.com/qq1624646454
 
-# For a User, please do the follows:
-cd ~/github
+${Fpink}# For a User, please do the follows: ${AC}
+cd ~/github.com/qq1624646454
 git clone https://github.com/qq1624646454/jllutils.git
+cd jllutils
 cd - >/dev/null
-
 
 
 # For a developer, please next to do the follows:
-cd ~/github/jllutils
-./jll.sshconf
+cd ~/github.com/qq1624646454/jllutils
+./jll.sshconf.sh
 cd - >/dev/null
+
+
+# Build the auto align from remote git repository
+tar -zvxf AUTO_TOOL_OVER_LINUX.tgz -C [YourTargetFolder]
+cd [YourTargetFolder]
+please modify ____install_auto_sync.sh for crontab triggerred actived time.
+./____install_auto_sync.sh
+
 
 
 EOF
