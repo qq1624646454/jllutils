9b519aa885e89b2efc8c18eeda70dfdd8842d092
jielong.lin <493164984@qq.com>
2017-06-28 15:16:03 +0800

    Changes as follows:  M jll.manual.git_repo.sh

--- jll.manual.git_repo.sh
+++ jll.manual.git_repo.sh
@@ -7,9 +7,19 @@ source ${JLLPATH}/BashShellLibrary
 
 more >&1 << EOF
 
-***************************************************
-** How to Use git & repo in Philips TV Project
-***************************************************
+******************************************************************
+** How to build git project management:
+**   Linux(Debian,Ubuntu) as git remote repository
+**   Window7(installed Git-Bash, ConEmu) as git local repository
+**
+** Such as Android APP code development workaround in windows7
+******************************************************************
+##
+## Install git remote repository
+\$${AC}${Fyellow} ${AC}
+
+
+
 
 
 
