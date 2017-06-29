b5b10da95b1812372dc8490e30c302924c693eb1
jielong.lin <493164984@qq.com>
2017-06-29 09:50:03 +0800

    Changes as follows:  M jll.manual.git_repository.sh

--- jll.manual.git_repository.sh
+++ jll.manual.git_repository.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-06-28 16:43:38
-#   ModifiedTime: 2017-06-29 09:30:50
+#   ModifiedTime: 2017-06-29 09:49:34
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -195,6 +195,23 @@ To 172.20.30.29:drmplayer_demo
 
 jielong.lin@XMNB4003161 MINGW32 /d/System/jielong.lin/Desktop/hello (master)
 \$
+
+
+${Bred}${Fwhite}                                                               ${AC}
+${Bred}${Fwhite}  ${AC}  ISSUE-$((issueID++)) on git version 2.9.3.windows.2
+${Bred}${Fwhite}                                                               ${AC}
+${Fwhite}jielong.lin@XMNB4003161 MINGW32 ~/hello \$${Fyellow} git add .        ${AC}
+${Fred}warning: LF will be replaced by CRLF in hello.txt. ${AC}
+${Fred}The file will have its original line endings in your working directory. ${AC}
+
+${Bgreen}${Fblack}SOLVE ${AC}
+${Fwhite}root@TpvServer:/etc/apache2# ${Fgreen}vim /etc/apache2/ports.conf${AC}
+  7
+  8 NameVirtualHost *:80
+  9 Listen ${Fgreen}8888${AC}
+ 10
+
+
 
 
 EOF
