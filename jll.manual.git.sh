5f88940f5890c0b06d2532b8f38a69be47eb69d1
jielong.lin <493164984@qq.com>
2017-07-05 09:37:04 +0800

    Changes as follows:  M jll.manual.git.sh

--- jll.manual.git.sh
+++ jll.manual.git.sh
@@ -175,7 +175,6 @@ git reset HEAD 如果后面什么都不跟的话 就是上一次add 里面的全
 git reset HEAD XXX/XXX/XXX.java 就是对某个文件进行撤销了
 
 2. git commit 错误
-
 如果不小心 弄错了 git add后 ， 又 git commit 了。 
 先使用 
 git log 查看节点 
@@ -183,26 +182,23 @@ commit xxxxxxxxxxxxxxxxxxxxxxxxxx
 Merge: 
 Author: 
 Date: 
-
 然后 
 git reset commit_id
 
 over
-
 PS：还没有 push 也就是 repo upload 的时候
 
 git reset commit_id （回退到上一个 提交的节点 代码还是原来你修改的） 
 git reset –hard commit_id （回退到上一个commit节点， 代码也发生了改变，变成上一次的）
 
 3.如果要是 提交了以后，可以使用 git revert
-
 还原已经提交的修改 
 此次操作之前和之后的commit和history都会保留，并且把这次撤销作为一次最新的提交 
 git revert HEAD 撤销前一次 commit 
 git revert HEAD^ 撤销前前一次 commit 
 git revert commit-id (撤销指定的版本，撤销也会作为一次提交进行保存） 
 git revert是提交一个新的版本，将需要revert的版本的内容再反向修改回去，版本会递增，不影响之前提交的内容。
-)
+
 
 
 
