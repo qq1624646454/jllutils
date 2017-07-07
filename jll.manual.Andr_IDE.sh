1b4a02c207949b8cfaf42dd7a897506f1e4c7e3a
jielong.lin <493164984@qq.com>
2017-07-07 09:23:08 +0800

    Changes as follows: ?? jll.manual.Andr_IDE.sh

---
+++ jll.manual.Andr_IDE.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  jielong.lin.  All rights reserved.
+#
+#   FileName:     jll.manual.Andr_IDE.sh
+#   Author:       jielong.lin
+#   Email:        493164984@qq.com
+#   DateTime:     2017-07-07 09:22:27
+#   ModifiedTime: 2017-07-07 09:22:43
+
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+source ${JLLPATH}/BashShellLibrary
+
+more >&1<<EOF
+
+
+
+EOF
+
