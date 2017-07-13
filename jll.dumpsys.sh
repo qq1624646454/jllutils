8bbd8b996e6a9894c1c3b5844580988061827a05
jielong.lin <493164984@qq.com>
2017-07-13 18:38:05 +0800

    Changes as follows:  M jll.dumpsys.sh

--- jll.dumpsys.sh
+++ jll.dumpsys.sh
@@ -75,7 +75,7 @@ echo
 if [ -e "/etc/X11/default-display-manager" ]; then
 echo -ne "${Fseablue}当前使用的显示管理器:${AC}   "
 cat  /etc/X11/default-display-manager
-echo -e "${Fgreen}Configuration: ${AC} @/etc/X11/default-display-manager"
+echo -e "${Fgreen}Howto_Set_Display_Manager: ${AC} /etc/X11/default-display-manager"
 echo
 fi
 echo
