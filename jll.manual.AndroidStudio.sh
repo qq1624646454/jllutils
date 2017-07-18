36c59eda011d03350caf3a8636341e23a971694b
jielong.lin <493164984@qq.com>
2017-07-18 09:32:05 +0800

    Changes as follows:  M jll.manual.AndroidStudio.sh

--- jll.manual.AndroidStudio.sh
+++ jll.manual.AndroidStudio.sh
@@ -5,26 +5,13 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-05-23 14:08:54
-#   ModifiedTime: 2017-07-18 09:31:11
+#   ModifiedTime: 2017-07-18 09:32:01
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
 source ${JLLPATH}/BashShellLibrary
 
 more >&1<<EOF
-
-${Bred}${Black}                                     ${AC}
-${Bred}  ${AC} 定制任意路径下的Android模拟器运行环境
-${Bred}  ${AC} 注意:在Vcxsrv环境中，似乎还无法支持硬件加速的GLE，因此，请设置-gpu swiftshader
-${Bred}${Black}                                     ${AC}
-${Fyellow}将以下脚本写入~/.bashrc，并重新登录即可${AC}
-alias as_emulator="ls ~/.android/avd | awk -F'.ini' '{print \$1}' | awk -F'.avd' '{print \$1}' \
-| uniq;
-read -p \"please type AVD=\" AVD_name;
-[ x\"\${AVD_name}\" != x ] \
-&& ~/dl.google.com/android/repository/emulator/emulator -avd \"\${AVD_name}\" -gpu swiftshader \
-"
-
 
 ${Bred}${Black}                                     ${AC}
 ${Bred}  ${AC} Install Android Studio 2.3.3 in Ubuntu 12.04 64bit. 
@@ -54,13 +41,25 @@ sudo apt-get install libnotify-dev
 
 
 
+${Bred}${Black}                                     ${AC}
+${Bred}  ${AC} 定制任意路径下的Android模拟器运行环境
+${Bred}  ${AC} 注意:在Vcxsrv环境中，似乎还无法支持硬件加速的GLE，因此，请设置-gpu swiftshader
+${Bred}${Black}                                     ${AC}
+${Fyellow}将以下脚本写入~/.bashrc，并重新登录即可${AC}
+alias as_emulator="ls ~/.android/avd | awk -F'.ini' '{print \$1}' | awk -F'.avd' '{print \$1}' \
+| uniq;
+read -p \"please type AVD=\" AVD_name;
+[ x\"\${AVD_name}\" != x ] \
+&& ~/dl.google.com/android/repository/emulator/emulator -avd \"\${AVD_name}\" -gpu swiftshader \
+"
+
+
 
 ${Bred}${Black}                                     ${AC}
 ${Bred}  ${AC}  AVD运行时错误
 ${Bred}  ${AC} ${Fred}A solution for pulseaudio: pa_context_connect() failed with QEMU${AC}
 ${Bred}${Black}                                     ${AC}
 sudo aptitude install pulseaudio
-
 
 
 
