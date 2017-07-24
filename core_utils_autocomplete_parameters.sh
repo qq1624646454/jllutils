5b2add05463e3b3ac01ab28c7670a73a1733bf25
jielong.lin <493164984@qq.com>
2017-07-24 15:44:06 +0800

    Changes as follows:  M core_utils_autocomplete_parameters.sh

--- core_utils_autocomplete_parameters.sh
+++ core_utils_autocomplete_parameters.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-04-28 15:42:49
-#   ModifiedTime: 2017-07-24 15:41:30
+#   ModifiedTime: 2017-07-24 15:43:07
 #
 # Abbreviation: cuap
 # source core_utils_autocomplete_parameters.sh in ~/.bashrc
