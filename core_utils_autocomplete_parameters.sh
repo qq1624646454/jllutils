41447502a7d919b98206700a43b564345cf54f79
jielong.lin <493164984@qq.com>
2017-07-25 09:56:06 +0800

    Changes as follows:  M core_utils_autocomplete_parameters.sh

--- core_utils_autocomplete_parameters.sh
+++ core_utils_autocomplete_parameters.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-04-28 15:42:49
-#   ModifiedTime: 2017-07-25 09:55:00
+#   ModifiedTime: 2017-07-25 09:56:01
 #
 # Abbreviation: cuap
 # source core_utils_autocomplete_parameters.sh in ~/.bashrc
@@ -162,12 +162,15 @@ function _____cuap__vicc()
     # COMP_CWORD is the system variable, it implies the current command keyword index.
     #    0: the first word
     case ${COMP_CWORD} in
+    0) # nothing to do
+        ;;
     1) # Command name has already been done, the first parameter can be started.
         if [ x"$(echo ${cur} | grep -E '^-')" != x ]; then
             __cmd_args="-l --list -c --create -d --delete -t --tag -u --update --auto"
         fi
         ;;
     *)
+        __cmd_args=""
         ;;
     esac
 
