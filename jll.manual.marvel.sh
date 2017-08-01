4b986b317548acdc6e1d91b04221a235a68cd656
jielong.lin <493164984@qq.com>
2017-08-01 15:20:06 +0800

    Changes as follows:  M jll.manual.marvel.sh

--- jll.manual.marvel.sh
+++ jll.manual.marvel.sh
@@ -5,7 +5,7 @@
 #   Author:       jielong.lin
 #   Email:        493164984@qq.com
 #   DateTime:     2017-08-01 15:16:23
-#   ModifiedTime: 2017-08-01 15:18:02
+#   ModifiedTime: 2017-08-01 15:19:09
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -13,19 +13,20 @@ source ${JLLPATH}/BashShellLibrary
 
 more >&1<<EOF
 
-Project Code is retrieved by the belows:
+${Fgreen}Project Code is retrieved by the belows:${AC}
          mkdir  -pv  bg2qr1_ppr2_119
          cd bg2qr1_ppr2_119
          repo init -u ssh://gerrit/platform/manifest -b tpvision/bg2qr1_ppr2_119
          repo sync -d
 
-Compile Commands as follows:
+${Fgreen}Compile Commands as follows:${AC}
    cd  bg2qr1_ppr2_119
    set_mvl
    source build/envsetup.sh
    lunch philipstv-userdebug
    ./device/tpvision/common/sde/upg/build_philipstv_A1.sh
-   ( don’t use option –t userdebug.  If you don’t use any option, by default it builds userdebug)  
+   ${Fseablue}( don’t use option –t userdebug.  ${AC}
+   ${Fseablue}  If you don’t use any option, by default it builds userdebug)${AC}
 
 
 
