225b50f0edfbc9513c6eebc6cb03d442511185b1
jielong.lin <493164984@qq.com>
2017-11-01 21:17:12 +0800

    Changes as follows:  M jll.ReachxmEnv_build.sh

--- jll.ReachxmEnv_build.sh
+++ jll.ReachxmEnv_build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Copyright(c) 2016-2100.  root.  All rights reserved.
 #
-#   FileName:     jll.ReachxmEnv_flash.sh
+#   FileName:     jll.ReachxmEnv_build.sh
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2017-11-01 21:11:31
-#   ModifiedTime: 2017-11-01 21:11:31
+#   ModifiedTime: 2017-11-01 21:15:42
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -16,7 +16,7 @@ JLLPATH="$(dirname ${JLLPATH})"
 # echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
 
 if [ -e "/media/root/work/mdm9607/mangov2/trunk_yxlog/apps_proc/poky" ]; then
-    cd /media/root/work/mdm9607/mangov2/trunk_yxlog/apps_proc/poky/bui
-    ./fl.sh
+    cd /media/root/work/mdm9607/mangov2/trunk_yxlog/apps_proc/poky 
+     
 fi
 
