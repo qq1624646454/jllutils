f4e13e32ca43c0d15d466538546b700f096b66de
jielong.lin <493164984@qq.com>
2017-11-01 21:38:08 +0800

    Changes as follows:  M jll.ReachxmEnv_build_kernel.sh

--- jll.ReachxmEnv_build_kernel.sh
+++ jll.ReachxmEnv_build_kernel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Copyright(c) 2016-2100.  root.  All rights reserved.
 #
-#   FileName:     jll.ReachxmEnv_build_kernel.sh
+#   FileName:     jll_source_it_TO_BUILD_KERNEL_FROM_yxlog.sh 
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2017-11-01 21:11:31
-#   ModifiedTime: 2017-11-01 21:18:13
+#   ModifiedTime: 2017-11-01 21:36:04
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -16,9 +16,13 @@ JLLPATH="$(dirname ${JLLPATH})"
 # echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
 
 if [ -e "/media/root/work/mdm9607/mangov2/trunk_yxlog/apps_proc/poky" ]; then
-    cd /media/root/work/mdm9607/mangov2/trunk_yxlog/apps_proc/poky 
-    source build/conf/set_bb_env_L170H.sh
-    mbuild linux-quic
+    if [ "$(pwd)" != "/media/root/work/mdm9607/mangov2/trunk_yxlog/apps_proc/poky/build" ]; then
+        cd /media/root/work/mdm9607/mangov2/trunk_yxlog/apps_proc/poky 
+        source build/conf/set_bb_env_L170H.sh
+        mbuild linux-quic
+    else
+        mbuild linux-quic
+    fi
 else
 cat >&1<<EOF
     cd /media/root/work/mdm9607/mangov2/trunk_yxlog/apps_proc/poky 
