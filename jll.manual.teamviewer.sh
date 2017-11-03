2997634853285cd50a567ed325d46e93e386aae0
jielong.lin <493164984@qq.com>
2017-11-03 20:13:24 +0800

    Commited by using jll.Git.sh @ 2017-11-03 20:13:24
    Changes as follows:
    A  jll.manual.teamviewer.sh

---
+++ jll.manual.teamviewer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  root.  All rights reserved.
+#
+#   FileName:     jll.manual.teamviewer.sh
+#   Author:       root
+#   Email:        493164984@qq.com
+#   DateTime:     2017-11-03 20:11:44
+#   ModifiedTime: 2017-11-03 20:11:44
+
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+#source ${JLLPATH}/BashShellLibrary
+
+### Color Echo Usage ###
+# Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
+# echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
+
+more >&1<<EOF
+
+Account: english name qq mail
+Password: X[IL6][domain10_lowercase]
+
+EOF
+
