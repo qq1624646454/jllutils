cef29d326716a90e7ec1efa791e31bf828c527bf
jielong.lin <493164984@qq.com>
2017-11-04 21:58:29 +0800

    Commited by using jll.Git.sh @ 2017-11-04 21:58:29
    Changes as follows:
    A  jll.manual.test.sh

---
+++ jll.manual.test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  root.  All rights reserved.
+#
+#   FileName:     jll.manual.test.sh
+#   Author:       root
+#   Email:        493164984@qq.com
+#   DateTime:     2017-11-04 21:58:22
+#   ModifiedTime: 2017-11-04 21:58:22
+
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+#source ${JLLPATH}/BashShellLibrary
+
+### Color Echo Usage ###
+# Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
+# echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
+
+
