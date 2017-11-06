1fe2e5f0e8e9551713c3c854ae00634673fd6d42
jielong.lin <493164984@qq.com>
2017-11-07 00:19:15 +0800

    Changes as follows: ?? jll.manual.usb_camera.sh

---
+++ jll.manual.usb_camera.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  root.  All rights reserved.
+#
+#   FileName:     jll.manual.usb_camera.sh
+#   Author:       root
+#   Email:        493164984@qq.com
+#   DateTime:     2017-11-07 00:18:08
+#   ModifiedTime: 2017-11-07 00:18:08
+
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+#source ${JLLPATH}/BashShellLibrary
+
+### Color Echo Usage ###
+# Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
+# echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
+
+more >&1<<EOF
+
+
+EOF
+
