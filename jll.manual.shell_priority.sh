3073b5c0e7e367a2a3629bdec96ae1dad101f9c8
jielong.lin <493164984@qq.com>
2017-11-07 18:16:07 +0800

    Changes as follows:  M jll.manual.shell_priority.sh

--- jll.manual.shell_priority.sh
+++ jll.manual.shell_priority.sh
@@ -5,7 +5,7 @@
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2017-11-07 18:14:14
-#   ModifiedTime: 2017-11-07 18:14:14
+#   ModifiedTime: 2017-11-07 18:14:57
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -18,6 +18,13 @@ JLLPATH="$(dirname ${JLLPATH})"
 more >&1<<EOF
 
 通俗地讲，linux系统中，进程有-19到19这39个优先级。-19最优先，19最不优先。进程的默认优先级为0。
+
+# 最低优先级
+nice -19 tar zcf pack.tar.gz documents
+
+# 最高优先级
+nice --19 tar zcf pack.tar.gz documents
+
 
 EOF
 
