f10279697be17943a6ff308ad29922ed194e6012
jielong.lin <493164984@qq.com>
2017-11-09 00:00:09 +0800

    Changes as follows: ?? jll.manual.power_manager.sh

---
+++ jll.manual.power_manager.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  root.  All rights reserved.
+#
+#   FileName:     jll.manual.power_manager.sh
+#   Author:       root
+#   Email:        493164984@qq.com
+#   DateTime:     2017-11-08 22:23:41
+#   ModifiedTime: 2017-11-08 22:23:41
+
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+#source ${JLLPATH}/BashShellLibrary
+
+### Color Echo Usage ###
+# Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
+# echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
+
+cat >&1<<EOF
+
+https://www.cnblogs.com/sky-heaven/p/5736882.html
+
+EOF
+
