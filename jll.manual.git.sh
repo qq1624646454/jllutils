4433d5cc938a8eb589efba17d92142868fe94aba
root <root@debian.DebianDomain>
2017-11-24 21:49:26 +0800

    Commited by using jll.Git.sh @ 2017-11-24 21:49:26
    Changes as follows:
    M  jll.manual.git.sh

--- jll.manual.git.sh
+++ jll.manual.git.sh
@@ -302,5 +302,13 @@ ${Fred} Agent admitted failure to sign using the key  ${AC}
 ssh-add ~/.ssh/id_rsa
 
 
+
+${Fred} gitignore文件中的规则不起作用${AC}
+原因是因为在Studio的git忽略目录中，新建的文件在git中会有缓存，如果某些文件已经被纳入了版本管理中，就算是在.gitignore中已经声明了忽略路径也是不起作用的，这时候我们就应该先把本地缓存删除，然后再进行git的push，这样就不会出现忽略的文件了。git清除本地缓存命令如下
+
+git rm -r --cached .
+git add .
+git commit -m 'update .gitignore'
+
 EOF
 
