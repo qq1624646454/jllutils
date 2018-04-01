6c466faf1e95bbf985c1cb29af90f6215174e7e9
jielong.lin <493164984@qq.com>
2018-04-01 21:38:22 +0800

    Changes as follows:  M jll.manual.ubifs.sh

--- jll.manual.ubifs.sh
+++ jll.manual.ubifs.sh
@@ -5,7 +5,7 @@
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2018-03-29 17:19:01
-#   ModifiedTime: 2018-04-01 21:36:08
+#   ModifiedTime: 2018-04-01 21:37:35
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -59,7 +59,9 @@ ubidetach /dev/ubi_ctrl -m 0
 #(5).格式化：意思就是把镜像文件按照/dev/mtd0的页大小等硬件flash格式烧写到这个设备空间中.注意：这里要加上 -O 2048 的选项，
 #            显式表明 UBI_VID_HDR 的偏移位置是 2KB，而不是默认值。从上面 mtdinfo /dev/mtd0 的输出结果中，有一项
 #            Sub-page size 的选项，如果不用 -O 显示指定，默认偏移值则是 sub-page size
-ubiformat /dev/mtd0 -s 2048 -f Angstrom-x11-at91sam9-image-eglibc-ipk-v20110624-at91sam9x5ek.rootfs.ubi
+ubiformat /dev/mtd0 -s 2048 -f mdm9607-sysfs.ubi
+
+#(6).重新绑定/dev/ubi_ctrl和/dev/mtd0
 ubiattach /dev/ubi_ctrl -m 0 -O 2048
 mkdir /mnt/loop
 mount -t ubifs ubi0 /mnt/loop
