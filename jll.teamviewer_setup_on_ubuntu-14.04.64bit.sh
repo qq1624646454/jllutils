f91b4a2fb5645c13ebf61055875ddcdb8331fc01
jielong.lin <493164984@qq.com>
2018-08-05 12:00:43 +0800

    Changes as follows:  M jll.teamviewer_setup_on_ubuntu-14.04.64bit.sh  D teamviewer_i386/teamviewer_i386.deb ?? teamviewer/

--- jll.teamviewer_setup_on_ubuntu-14.04.64bit.sh
+++ jll.teamviewer_setup_on_ubuntu-14.04.64bit.sh
@@ -5,7 +5,7 @@
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2017-12-25 14:15:05
-#   ModifiedTime: 2017-12-25 14:15:05
+#   ModifiedTime: 2018-08-02 23:00:28
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -15,7 +15,22 @@ JLLPATH="$(dirname ${JLLPATH})"
 # Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
 # echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
 
-if [ -e "${JLLPATH}/JLink/JLink_Linux_V630k_x86_64.deb" ]; then
+if [ -e "${JLLPATH}/teamviewer/teamviewer_13.1.8286_amd64.deb" ]; then
+    aptitude install -f libqt5x11extras5-dev
+    dpkg -i ${JLLPATH}/teamviewer/teamviewer_13.1.8286_amd64.deb
+    if [ x"$(aptitude search teamviewer | grep -E '^i')" = x ]; then
+        apt-get update
+        apt-get -f install
+        if [ x"$(aptitude search teamviewer | grep -E '^i')" != x ]; then
+            exit 0 
+        fi
+    fi
+else
+    echo -e "${Fred}JLLim| Sorry, not found ${JLLPATH}/teamviewer/teamviewer_13.1.8286_amd64.deb${AC}"
+    echo -e "${Fred}JLLim| Try to install ${JLLPATH}/teamviewer/teamviewer_i386.deb${AC}"
+fi
+
+if [ -e "${JLLPATH}/teamviewer/teamviewer_i386.deb" ]; then
     dpkg --add-architecture i386
     apt-get update
     apt-get -f install
@@ -36,8 +51,8 @@ if [ -e "${JLLPATH}/JLink/JLink_Linux_V630k_x86_64.deb" ]; then
                     libxrender1:i386 \
                     libxtst6:i386 \
                     zlib1g:i386
-    dpkg -i ${JLLPATH}/teamviewer_i386/teamviewer_i386.deb
+    dpkg -i ${JLLPATH}/teamviewer/teamviewer_i386.deb
 else
-    echo -e "${Fred}JLLim| Sorry, not found ${JLLPATH}/teamviewer_i386/teamviewer_i386.deb${AC}"
+    echo -e "${Fred}JLLim| Sorry, not found ${JLLPATH}/teamviewer/teamviewer_i386.deb${AC}"
 fi
 
