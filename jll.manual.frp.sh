82a1c4a7730d333d511457c592ee0b13bac70d25
jielong.lin <493164984@qq.com>
2019-01-23 00:14:12 +0800

    Changes as follows:  M jll.manual.frp.sh

--- jll.manual.frp.sh
+++ jll.manual.frp.sh
@@ -2,38 +2,14 @@
 # Copyright(c) 2016-2100.  root.  All rights reserved.
 #
 #   FileName:     jll.manual.frp.sh
-<<<<<<< HEAD
-#   Author:       root
-#   Email:        493164984@qq.com
-#   DateTime:     2019-01-22 23:21:45
-#   ModifiedTime: 2019-01-22 23:22:07
-
-JLLPATH="$(which $0)"
-JLLPATH="$(dirname ${JLLPATH})"
-#source ${JLLPATH}/BashShellLibrary
-
-### Color Echo Usage ###
-# Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
-# echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
-=======
 #   Author:       JLLim 
 #   Email:        493164984@qq.com
 #   DateTime:     2019-01-22 23:12:35
-#   ModifiedTime: 2019-01-22 23:39:55
->>>>>>> 176d226fd3dbaada0976067579a74f9cf44ae550
+#   ModifiedTime: 2019-01-23 00:13:55
 
 more >&1<<EOF
 
 =======================================================
-<<<<<<< HEAD
-      
--------------------------------------------------------
-1.
-
-EOF
-
-
-=======
   基于免费frps服务器搭建内网透传网络
 -------------------------------------------------------
 
@@ -63,35 +39,32 @@ EOF
 
         [common]
 
-#frps服务端地址
-server_addr = freenat.bid 
-
-#frps服务端通讯端口，客户端连接到服务端内网穿透传输数据的端口
-server_port = 7000
-
-#特权模式密钥，客户端连接到FRPS服务端的验证密钥
-privilege_token = frp888
-
-#日志存放路径
-log_file = /root/frpc.log
-
-#日志记录类别,可选：trace, debug, info, warn, error
-#log_level = trace
-log_level = info
-
-#日志保存天数
-log_max_days = 7 
-
-#login_fail_exit = true 
-#设置为false，frpc连接frps失败后重连，默认为true不重连
-login_fail_exit = false
-
-#KCP协议在弱网环境下传输效率提升明显，但是对frps会有一些额外的流量消耗。
-#服务端须先设置kcp_bind_port = 7000，freenat.bid服务端已设置支持
-protocol = kcp 
-
-
+        #frps服务端地址
+        server_addr = freenat.bid 
+        
+        #frps服务端通讯端口，客户端连接到服务端内网穿透传输数据的端口
+        server_port = 7000
+        
+        #特权模式密钥，客户端连接到FRPS服务端的验证密钥
+        privilege_token = frp888
+        
+        #日志存放路径
+        log_file = /root/frpc.log
+        
+        #日志记录类别,可选：trace, debug, info, warn, error
+        #log_level = trace
+        log_level = info
+        
+        #日志保存天数
+        log_max_days = 7 
+        
+        #login_fail_exit = true 
+        #设置为false，frpc连接frps失败后重连，默认为true不重连
+        login_fail_exit = false
+        
+        #KCP协议在弱网环境下传输效率提升明显，但是对frps会有一些额外的流量消耗。
+        #服务端须先设置kcp_bind_port = 7000，freenat.bid服务端已设置支持
+        protocol = kcp 
 
 EOF
->>>>>>> 176d226fd3dbaada0976067579a74f9cf44ae550
 
