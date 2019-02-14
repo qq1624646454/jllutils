792f391653f7a0331387c3a9df952985b6ba7fe8
jielong.lin <493164984@qq.com>
2019-02-15 00:00:25 +0800

    Changes as follows: ?? jll.manual.docker.sh

---
+++ jll.manual.docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  root.  All rights reserved.
+#
+#   FileName:     jll.manual.docker.sh
+#   Author:       root
+#   Email:        493164984@qq.com
+#   DateTime:     2019-02-14 15:23:40
+#   ModifiedTime: 2019-02-14 15:28:37
+
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+#source ${JLLPATH}/BashShellLibrary
+
+### Color Echo Usage ###
+# Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
+# echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
+
+more>&1<<EOF
+
+-----------------------------------
+INSTALL on Ubuntu-14 (64bit):
+    https://docs.docker.com/install/linux/docker-ce/ubuntu/
+-----------------------------------
+
+
+
+
+EOF
+
