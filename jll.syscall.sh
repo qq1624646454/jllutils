db4a670c6d7e2b6f180d0e5435c5c861fd8a0758
reach <reach@reachxm.com>
2019-05-25 17:13:41 +0800

    Commited by using jll.Git.sh @ 2019-05-25 17:13:41
    Changes as follows:
    M  jll.syscall.sh

--- jll.syscall.sh
+++ jll.syscall.sh
@@ -5,7 +5,7 @@
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2019-05-25 16:10:53
-#   ModifiedTime: 2019-05-25 16:21:05
+#   ModifiedTime: 2019-05-25 17:13:28
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -17,22 +17,77 @@ JLLPATH="$(dirname ${JLLPATH})"
 
 Usage()
 {
-    echo
-    echo "JLLim| USAGE:"
-    echo "JLLim|"
-    echo "JLLim|     ${0##*/} <syscall-function-name>"
-    echo "JLLim|"
-    echo "JLLim| FOR EXAMPLE:"
-    echo "JLLim|     sw/L170L_baseline/apps_proc/kernel# jll.syscall.sh socket"
-    echo "JLLim|"
-    echo "JLLim|"
-    echo
+more >&1<<EOF
+
+  USAGE:
+    ${0##*/} <syscall-function-name>
+
+    ${0##*/} <syscall-function-name> [ignored_subdirectory_list]
+ 
+  ForExample:
+    sw/L170L_baseline/apps_proc/kernel# jll.syscall.sh socket
+
+    sw/L170L_baseline/apps_proc/kernel# jll.syscall.sh socket tools Documentation include
+
+EOF
+
 }
 
-if [ $# -ne 1 ]; then
-    Usage
-    exit 0
+if [ $# -lt 1 ]; then
+  Usage
+  exit 0
 fi
 
-find . -type f -a -name *.c | xargs -i grep "SYSCALL_DEFINE[0-9]\{1,\}[ ]\{0,\}(" -nH {} | grep "$1[ ]\{0,\}," --color
+start=$(date +%s%N)
+start_ms=${start:0:16}
 
+if [ $# -eq 1 ]; then
+  find . -type f -a -name *.c | xargs -i grep "SYSCALL_DEFINE[0-9]\{1,\}[ ]\{0,\}(" -nH {} \
+    | grep "$1[ ]\{0,\}," --color
+else
+  CONF_ARGS1="-path ./$2"
+  for((i=3;i<=$#;i++)) {
+    CONF_ARGS1="${CONF_ARGS1} -o -path "./$(eval echo "$"$i)
+  }
+
+#  find . \( ${CONF_ARGS1} \) -prune \
+#         -o \( -type f  -a  -name '*.C' -print \
+#         -o -name '*.c' -print \
+#         -o -name '*.cpp' -print \
+#         -o -name '*.cc' -print \
+#         -o -name '*.vc' -print \
+#         -o -name '*.s' -print \
+#         -o -name '*.S' -print \
+#         -o -name '*.h' -print \
+#         -o -name '*.H' -print \
+#         -o -name '*.java' -print \) \
+#    | xargs -i grep "SYSCALL_DEFINE[0-9]\{1,\}[ ]\{0,\}(" -nH {} \
+#    | grep "$1[ ]\{0,\}," --color
+
+  find . \( ${CONF_ARGS1} \) -prune \
+         -o \( -type f  -a  -name '*.C' -print \
+         -o -name '*.c' -print \
+         -o -name '*.cpp' -print \
+         -o -name '*.s' -print \
+         -o -name '*.S' -print \) \
+    | xargs -i grep "SYSCALL_DEFINE[0-9]\{1,\}[ ]\{0,\}(" -nH {} \
+    | grep "$1[ ]\{0,\}," --color
+
+  [ x"${CONF_ARGS1}" != x ] && unset CONF_ARGS1
+fi
+
+end=$(date +%s%N)
+end_ms=${end:0:16}
+cost_time=$(echo "scale=6;($end_ms - $start_ms)/1000000" | bc)
+
+echo
+echo "=========================================="
+echo "JLLim| COST TIME is: ${cost_time} s"
+echo "=========================================="
+echo
+
+[ x"${start}" != x ] && unset start 
+[ x"${start_ms}" != x ] && unset start_ms 
+[ x"${end}" != x ] && unset end 
+[ x"${end_ms}" != x ] && unset end_ms 
+[ x"${cost_time}" != x ] && unset cost_time
