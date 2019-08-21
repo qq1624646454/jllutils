043fcc9e505ef2ef683c3200f9098d1a6fcc31d6
jielong.lin <493164984@qq.com>
2019-08-22 01:00:09 +0800

    Commited by using jll.Git.sh @ 2019-08-22 01:00:09
    Changes as follows:
    A  .test_indent/test_style.c
    A  jll.manual.indent.sh

---
+++ .test_indent/test_style.c
@@ -0,0 +1,112 @@
+/*
+ * Copyright(c) 2016-2100.  root.  All rights reserved.
+ */
+/*
+ * FileName:      1.c
+ * Author:        root
+ * Email:         493164984@qq.com
+ * DateTime:      2019-07-25 19:38:37
+ * ModifiedTime:  2019-08-22 00:33:18
+ */
+
+
+#include <stdio.h>
+/*
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+*/
+/*********************************************************************************************************************************/
+/*
+ *
+ */
+
+/*
+
+hello
+    world
+
+ */
+int main(int argc
+
+
+, char *argv[])
+{
+  struct xxxxxxxxxxxxxxxx     y;
+  struct 
+xxxxxxxxxxxxxxxx     z;
+ //hello
+int j,i=0,k=1;
+ /* hello*/
+    printf("argc=%d, argv[0]=%s, argv[1]=%s, argv[2]=%s ------------------------------------------------------------------\r\n", argc, argv[0], argv[1], argv[2]);
+/*
+                                * hell   xxx
+                                   * xxx
+
+
+    */
+    printf("argc=%d, argv[0]=%s, argv[1]=%s, argv[2]=%s --------------------------\r\n", argc, argv[0], argv[1], argv[2]);
+    if(i==1){
+  printf("i==1\r\n");                      /*
+                                             *
+                                           *    yes yes
+                                            */
+    } else	 if     (i==2)
+
+
+
+
+	
+
+
+{
+		 printf("i==2\r\n");return 0;
+    
+    }
+ else 
+{
+  printf("hello\r\n"
+) ;
+}
+/*
+                                * hell   xxx
+                                   * xxx
+   
+    */
+   /*
+                                 hell   xxx
+                                    xxx
+   
+    */
+
+
+
+    if (k==1 && j > 1 || (i++ > 1)
+|| (k+j > 0)) {
+printf("hello\r\n");
+}
+
+
+    switch(i)
+
+{
+case 1:
+   break;
+	 case     2   :
+	break;
+}
+ for(i=0;i<1   ;i++){
+j=i  ;
+     }
+
+while(k==100)
+{ k+=2;
+ k+=2;
+}
+	return 0;
+}
+
