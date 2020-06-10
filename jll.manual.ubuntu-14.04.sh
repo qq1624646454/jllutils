f2390cc693ffe4e913c4d7cca26860867018967b
jielong.lin <493164984@qq.com>
2020-06-11 00:34:07 +0800

    Changes as follows:  M jll.manual.ubuntu-14.04.sh

--- jll.manual.ubuntu-14.04.sh
+++ jll.manual.ubuntu-14.04.sh
@@ -5,7 +5,7 @@
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2020-06-11 00:16:15
-#   ModifiedTime: 2020-06-11 00:31:25
+#   ModifiedTime: 2020-06-11 00:33:26
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -22,6 +22,86 @@ ${Fred} How to download Ubuntu 14.04 ${AC}
 Open link to https://www.ubuntu.com/download/alternative-downloads
 Download BitTorrent and Open BitTorrent to Download Ubuntu image is very fast
 I recommend that it is performed by firefox on ubuntu 14.04
+
+
+
+${Fseablue}Waiting for network configuration…${AC}
+The eth0 is not linked on startup stage, so system will run /etc/init/failsafe.conf
+${Fred}Solved${AC}
+Solution-1: remove the unused eth0
+Solution-2: change the sleep time in /etc/init/failsafe.conf
+
+Note: wifi is started so slower is because eth0 is bad to delay checking.
+Hence please remove the unused eth0
+
+
+${Fwhite}${Bgreen} Auto-mount partitions  ${AC}
+\$ sudo vi /etc/fstab
+...
+# <file system> <mount point>   <type>  <options>          <dump>  <pass>
+  /dev/sda4     /fight4honor    ext4    errors=remount-ro  0       1
+  /dev/sda3     /ibbyte         ext4    errors=remount-ro  0       1
+  /dev/sda2     /my             ext4    defaults           0       2 
+:w
+
+
+${Fwhite}${Bgreen} stdin:is not tty when login ${AC}
+Error Found when loading /root/.profile:
+stdin: is not a tty
+${Fred}Solved${AC}
+edit /root/.profile
+modify the "mesg n" line to "tty -s && mesg n" then reboot system
+
+
+
+${Fwhite}${Bgreen} Enable root auto-login after installing Ubuntu-14.04 ${AC}
+\$ sudo passwd root
+${Bred}${Fyellow}for Ubuntu (using lightdm)${AC}
+# vi /etc/lightdm/lightdm.conf
+[SeatDefaults]
+autologin-guest=false
+autologin-user=root
+autologin-user-timeout=0
+autologin-session=lightdm-autologin
+user-session=ubuntu
+greeter-session=unity-greeter
+:w
+修改autologin-user=root
+
+# vi /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
+[SeatDefaults]
+greeter-session=unity-greeter
+
+
+
+${Fwhite}${Bgreen} AutoCompleted cmdline after installing Ubuntu-14.04 ${AC}
+Edit ~/.bashrc as follows:
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+    . /etc/bash_completion
+fi
+
+
+${Fwhite}${Bgreen} Install input method by fcitx wubi/pinyin ${AC} 
+# apt-get install fcitx-frontend-all fcitx-ui-classic fcitx-table-wubi
+# cp -rvf /etc/X11/xinit/xinputrc  /etc/X11/xinit/xinputrc.orig
+# im-config 
+JLL: Select fcitx and the update for /etc/X11/xinit/xinputrc
+# gnome-control-center
+JLL: select Input Sources About EN and CN
+# /usr/bin/fcitx-autostart
+# fcitx-configtool
+
+
+# reboot
+
+
+${Fgreen} Which desktop subsystem ${AC}
+    echo \$DESKTOP_SESSION
+
+
 
 
 
