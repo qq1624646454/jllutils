5c27344ab0c8185f274bf5635fecb1508d576d1b
jielong.lin <493164984@qq.com>
2021-02-01 12:00:23 +0800

    Changes as follows: ?? jll.manual.bilibili.com.sh

---
+++ jll.manual.bilibili.com.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright(c) 2016-2100.  root.  All rights reserved.
+#
+#   FileName:     jll.manual.bilibili.com.sh
+#   Author:       root
+#   Email:        493164984@qq.com
+#   DateTime:     2021-02-01 11:56:27
+#   ModifiedTime: 2021-02-01 11:56:27
+
+JLLPATH="$(which $0)"
+JLLPATH="$(dirname ${JLLPATH})"
+source ${JLLPATH}/BashShellLibrary
+
+### Color Echo Usage ###
+# Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
+# echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
+
+${Bblue}How to download the video by open-source ${AC}
+
+
