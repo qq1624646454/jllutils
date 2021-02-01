73a3e4c6ff792abb5a07112c266286534d680544
jielong.lin <493164984@qq.com>
2021-02-01 15:09:29 +0800

    Changes as follows:  M jll.manual.ubuntu-14.04.sh  D jll.manual.ubuntu-14.04.sh~

--- jll.manual.ubuntu-14.04.sh
+++ jll.manual.ubuntu-14.04.sh
@@ -5,7 +5,7 @@
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2020-06-11 00:16:15
-#   ModifiedTime: 2021-02-01 15:08:03
+#   ModifiedTime: 2021-02-01 15:08:27
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
