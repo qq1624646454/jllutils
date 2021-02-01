5e7536948d29898e5448a4cbaaee87dcbb4d0178
jielong.lin <493164984@qq.com>
2021-02-01 15:00:26 +0800

    Changes as follows:  M jll.manual.bilibili.com.sh

--- jll.manual.bilibili.com.sh
+++ jll.manual.bilibili.com.sh
@@ -5,7 +5,7 @@
 #   Author:       root
 #   Email:        493164984@qq.com
 #   DateTime:     2021-02-01 11:56:27
-#   ModifiedTime: 2021-02-01 11:56:27
+#   ModifiedTime: 2021-02-01 13:01:30
 
 JLLPATH="$(which $0)"
 JLLPATH="$(dirname ${JLLPATH})"
@@ -15,6 +15,13 @@ source ${JLLPATH}/BashShellLibrary
 # Lfn_Sys_ColorEcho ${CvFgRed} ${CvBgWhite} "hello"
 # echo -e "hello \033[0m\033[31m\033[43mworld\033[0m"
 
+more >&1<<EOF
+
 ${Bblue}How to download the video by open-source ${AC}
 
+pip3 install you-get
+pip3 install --upgrade you-get
 
+you-get https://www.bilibili.com/video/BV1MJ411h7Zy?from=search&seid=9339372455138228691
+
+EOF
